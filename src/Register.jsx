046ced8ae64a62625051c21b4d275d62f3e5ab25
juansuh <juansuhr@gmail.com>
2021-02-19 12:15:13 -0500

    error checking on register

--- src/Register.jsx
+++ src/Register.jsx
@@ -4,9 +4,22 @@ import { Box, Button, TextField } from "@material-ui/core";
 const RegisterPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  function errorChecking(username, password) {
+    const newErrors = []
+    if (username.length < 3) {
+      newErrors.push('Username too short! 3 chars minimum');
+    }
+    if (password.length < 5) {
+      newErrors.push('Password too short! 5 chars minimum')
+    }
+    setErrors(newErrors);
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     console.log(username);
     console.log(password);
   };
